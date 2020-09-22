d68b7e644efa3f86c26eb330439a4826da2797b0
Muhammad Shahzad <abisim32@gmail.com>
2020-09-22 21:05:23 +0500

    update intro.js
    Props Name was in running sentence

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 import Intro from './Intro.js';
 
 function App(){
-  return <div className="App"><Intro name="Muhammad Shahzad" /></div>;
+  return (
+  <div className="App"><Intro Name="Muhammad Shahzad" /></div>
+  );
 }
 
 export default App;
