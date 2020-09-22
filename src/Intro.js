1b63a91c74fde2e1b4431832df40f9b6178806bd
Muhammad Shahzad <abisim32@gmail.com>
2020-09-22 20:37:00 +0500

    add date in intro.js

--- src/Intro.js
+++ src/Intro.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Intro(props){
     return (
-    <h1>My name is {props.Name}. This is BOOTCAMP 2020 session 3 assignment.</h1>
+    <h1>My name is {props.Name}. This is BOOTCAMP 2020 session 3 assignment. created on 22 September 2020</h1>
     )
 }
 
